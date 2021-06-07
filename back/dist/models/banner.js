066bf0be43d12fe688d3539c6c2b47e4d4653170
최영재 <brandon94_@naver.com>
2021-06-07 19:20:01 +0900

    -

--- back/dist/models/banner.js
+++ back/dist/models/banner.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const BannerSchema = new mongoose_1.default.Schema({
-    user: {
+    image: {
         type: String,
         required: true
     }
